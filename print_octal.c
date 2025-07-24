68954dc7f9bc8b608833a7a259655911d777dbc8
ValentinDLC <valentin.dardenne9@gmail.com>
2025-07-24 14:24:52 +0200

    Add print_octal.c

---
+++ print_octal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_octal - Prints an unsigned integer in octal format
+ * @n: The unsigned integer to print
+ *
+ * Return: The number of characters printed
+ */
+int print_octal(unsigned int n)
+{
+	int len = 0;
+
+	if (n / 8) len += print_octal(n / 8);
+	len += print_char((n % 8) + '0');
+	return (len);
+}
