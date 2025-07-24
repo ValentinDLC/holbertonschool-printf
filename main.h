628ef4b94e6512d5059a3966bfa37c54956df619
ValentinDLC <valentin.dardenne9@gmail.com>
2025-07-24 14:21:08 +0200

    Add main.h (prototype)

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* Prototype for _printf */
+int _printf(const char *format, ...);
+
+/* Prototypes for display functions */
+int print_char(char c);
+int print_string(const char *s);
+int print_percent(void);
+int print_int(int n);
+int print_unsigned(unsigned int n);
+int print_octal(unsigned int n);
+int print_hex(unsigned int n, int uppercase);
+int print_pointer(void *ptr);
+
+#endif /* MAIN_H */
